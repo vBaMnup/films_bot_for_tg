bbee98e8ef15c8d5f7be4b2ade0f6695cd166657
Andrey Paskov <vbamnup@yandex.ru>
2022-08-19 17:11:23 +0300

    Дописал функцию парсинга прокси

--- tools/proxy.py
+++ tools/proxy.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import logging
 
 from config import PROXY_URL
 from user_agent import get_random_user_agent
@@ -12,3 +13,26 @@ def get_proxies():
     ).content, 'html.parser'
     )
     proxies = []
+    for row in parsed_proxies_list.find("div",
+                                        attrs={"class": "table_block"}
+                                        ).find("tbody").find_all("tr")[1:]:
+        tds = row.find_all('td')
+        try:
+            ip = tds[0].text.strip()
+            port = tds[1].text.strip()
+            country = tds[2].text.strip()
+            type_pr = tds[4].text.strip()
+            if type_pr == 'SOCKS4, SOCKS5':
+                continue
+            if country.startswith('Russian'):
+                continue
+            else:
+                host = f'{type_pr}://{ip}:{port}'
+            proxies.append(host)
+        except IndexError as error:
+            logging.error(error)
+    return proxies
+
+
+if __name__ == '__main__':
+    print(get_proxies())
